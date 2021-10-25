d93b89ac68844d06dd8a299e4d2859c8749f4c3f
gzhh <zhonghuangao@gmail.com>
2021-10-25 22:39:24 +0800

    Add channel and context cancel demo

---
+++ src/concurrency/context/README.go
@@ -0,0 +1,6 @@
+# Context
+### 介绍
+Package context defines the Context type, which carries deadlines, cancellation signals, and other request-scoped values across API boundaries and between processes.
+
+### 参考
+https://pkg.go.dev/context
---
+++ src/concurrency/context/cancel_by_context_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func isCancelled(ctx context.Context) bool {
+	select {
+	case <-ctx.Done():
+		return true
+	default:
+		return false
+	}
+}
+
+/*
+根Context：通过context.Background()创建
+子Context：context.WithCancel(parentContexl)创建
+  ctx, cancel := context.WithCancel(context.Background())
+当前Context被取消时，基于它的子context都会被取消
+接收消息通知<-ctx.Done
+*/
+func TestCancel(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	for i := 0; i < 5; i++ {
+		go func(i int, ctx context.Context) {
+			for {
+				if isCancelled(ctx) {
+					break
+				}
+				time.Sleep(time.Millisecond * 5)
+			}
+			fmt.Println(i, "Cancelled")
+		}(i, ctx)
+	}
+	cancel()
+	time.Sleep(time.Second * 1)
+}
