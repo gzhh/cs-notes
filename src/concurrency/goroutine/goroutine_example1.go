2796f9a09edff66545ad658aa2d2f4cfedd87e86
gzhh <zhonghuangao@gmail.com>
2022-05-18 10:48:11 +0800

    Add some goroutine demo

---
+++ src/concurrency/goroutine/goroutine_example1.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// 发送1000个请求，并控制10个并发
+// sync.WaitGroup + channel
+func main() {
+	var wg sync.WaitGroup
+	ch := make(chan struct{}, 10)
+	for i := 0; i < 1000; i++ {
+		ch <- struct{}{}
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			fmt.Println(i)
+			time.Sleep(time.Second)
+			<-ch
+		}(i)
+	}
+	wg.Wait()
+}
---
+++ src/concurrency/goroutine/goroutine_example2.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+/*
+使⽤两个goroutine交替打印序列，⼀个goroutine打印数字， 另外⼀个goroutine打印字⺟， 最终效果如下：
+12AB34CD56EF78GH910IJ1112KL1314MN1516OP1718QR1920ST2122UV2324WX2526YZ2728
+*/
+func main() {
+	number, letter := make(chan bool), make(chan bool)
+
+	go func() {
+		i := 1
+		for {
+			<-number
+			fmt.Printf("%d%d", i, i+1)
+			i += 2
+			letter <- true
+		}
+	}()
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func(wg *sync.WaitGroup) {
+		str := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		i := 0
+		for {
+			<-letter
+			if i >= 26 {
+				wg.Done()
+				return
+			}
+			fmt.Print(str[i : i+2])
+			i += 2
+			number <- true
+		}
+	}(&wg)
+
+	number <- true
+	wg.Wait()
+}
